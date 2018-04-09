e26e682bc5c147a61e0817efb6d28e9859138c40
Léa Blanchard <lea10.blanchard@gmail.com>
2018-04-09 11:25:22 +0200

    fenetre de connexion

--- src/Controleur/PROJET_HOPITAL.java
+++ src/Controleur/PROJET_HOPITAL.java
@@ -5,17 +5,46 @@
  */
 package Controleur;
 
+import Modele.Connexion;
+import java.sql.SQLException;
+
 /**
  *
  * @author lele
  */
 public class PROJET_HOPITAL {
 
+   /**
+     * ATTRIBUTES
+     */
+    private Fenetre fen; //fenetre graphique
+    private Connexion c; 
+    private String passwordDatabase;
+    private String loginDatabase;
+    private String nameDatabase;
+    private String userNameECE;
+    private String passwordECE;
+    
+    /**
+     * CONSTRUCTORS
+     */
+    PROJET_HOPITAL(){
+        fen= new Fenetre();
+        try{
+            c = new Connexion(userNameECE, passwordECE, loginDatabase, passwordDatabase);
+        }catch(SQLException e){
+            System.out.println("SQLException: "+e);
+        }catch(ClassNotFoundException e2){
+            System.out.println("ClassNotFoundException: "+e2);
+        }
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        PROJET_HOPITAL run= new PROJET_HOPITAL();
     }
     
 }
