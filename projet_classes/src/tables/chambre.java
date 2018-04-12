8a7ac9e40f040efe07b5935b8e21e7f3d5729ac1
Léa Blanchard <lea10.blanchard@gmail.com>
2018-04-12 10:43:19 +0200

    classes chambre et infirmier

---
+++ projet_classes/src/tables/chambre.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tables;
+
+/**
+ *
+ * @author lele
+ */
+public class chambre {
+    
+    //attributs 
+    private char code_service; 
+    private float no_chambre; 
+    private infirmier surveillant;
+    
+   
+    
+}
