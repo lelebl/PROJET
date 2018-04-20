0798975635e24ac7a5536e0613cb596b1314e694
Emmanuelle Thiroloix <emmanuelle.thiroloix@gmail.com>
2018-04-20 12:09:37 +0200

    constructeur MAJ

--- Projet_hopital_ING3/src/Vue/MAJPan.java
+++ Projet_hopital_ING3/src/Vue/MAJPan.java
@@ -1,18 +1,83 @@
 
 package Vue;
 
+import Modele.BDD.Database;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+
 
 public class MAJPan extends MyPanel{
     /**
      * ATTRIBUTES
      */
+    public String currentType=""; // supprimer, ajouter, modifier
+    public String currentTable=""; // table qu'on met à jour
+    private Database myData; //notre connexion a la BDD
+    private JButton boutons[]; // liste des tables de la BDD
+    private JPanel menuTable;
+    private JPanel pan;
+    private JLabel titre;
+    
+    private JComboBox listSupp;
+    
+    private JLabel[] labelAjout;
+    private JTextField[] textAjout;
+    private String[] saisie;
+    private JOptionPane errorInfo = new JOptionPane();
+    
+    private JButton valider;
+    
     
     /**
      * CONSTRUCTORS
      */
     public MAJPan(){
         super(false,"MAJ");
+        errorInfo.setSize(300,100);
+        errorInfo.setFont(new Font("Times New Roman", Font.BOLD,20));
+        // Déclaration des panels
+        menuTable=new JPanel();
+        pan= new JPanel();
+        LineBorder roundedLineBorder = new LineBorder(new Color(34, 66, 124), 10, true); // On crée la bordure des panels
+        menuTable.setBackground(new Color(255,255,255,150));
+        menuTable.setBorder(roundedLineBorder);
+        menuTable.setLayout(null);
+        pan.setBackground(new Color(255,255,255,150));
+        pan.setPreferredSize(new Dimension(this.getHeight()-100,this.getWidth()/5));
+        pan.setBorder(roundedLineBorder);
+        pan.setLayout(null);
+        
+        // Positionnement des labels:
+        this.setLayout(new GridBagLayout()); 
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.fill = GridBagConstraints.BOTH; //on occupe tout l'espace disponible
+	gc.ipady = gc.anchor = GridBagConstraints.CENTER; // on centre les composants
+        gc.weightx = 2; // 2 case en abscisse 
+	gc.weighty = 2; // 2 case en ordonnée
+        gc.insets = new Insets(50, 50, 50, 50); //on applique des marges
+        gc.gridx = 0;
+        gc.gridy = 1;
+        this.add(menuTable, gc);
+        gc.gridx=1;
+        gc.gridy=1;
+        this.add(pan, gc);
+        
+        boutons=null; //tant que l'on a pas de connexion on ne peut pas faire les boutons
+        myData=null; //car pas encore de connexion
+        this.setVisible(true);
     }
+    
     
     
 }
