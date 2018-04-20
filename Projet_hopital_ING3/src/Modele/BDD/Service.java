47e5d9069440fbd24fe7ae0eee48e93b49225673
Emmanuelle Thiroloix <emmanuelle.thiroloix@gmail.com>
2018-04-20 11:48:04 +0200

    service

---
+++ Projet_hopital_ING3/src/Modele/BDD/Service.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele.BDD;
+
+import Modele.Connexion;
+import java.awt.Color;
+import java.awt.Font;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Emmanuelle THIROLOIX
+ */
+public class Service implements Table {
+
+    private Connexion conn;
+    public String code;
+    public String nom;
+    public char batiment;
+    public int directeur;
+    public int nb_att;
+    public String[] attributs={"code", "nom", "batiment", "directeur"};;
+    public JTextField[] textAjout;
+    public JLabel[] labelAjout;
+    
+    public Service(String cod, String n, char bat, int dir, Connexion c){
+        code=cod;
+        nb_att=4;
+        nom=n;
+        batiment=bat;
+        directeur=dir;
+        conn=c;
+        this.initLabelsAjout();
+    }
+
+    public void initLabelsAjout(){
+        textAjout= new JTextField[nb_att];
+        labelAjout= new JLabel[nb_att];
+        
+        for(int i=0; i<nb_att;i++){
+            labelAjout[i]=new JLabel();
+            labelAjout[i].setSize(300, 25);
+            labelAjout[i].setFont(new Font("Times New Roman", Font.BOLD,20));
+            labelAjout[i].setForeground(new Color(3,34,76));
+            labelAjout[i].setText(attributs[i]);
+            textAjout[i]=new JTextField();
+        }
+    }
+     
+    @Override
+    public String toString(){
+        return ("code: "+code+" / nom: "+nom+" / batiment: "+batiment+" / directeur: "+directeur);
+    }
+
+    
+
+    @Override
+    public void ajouter(String attribut) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean modifier(ArrayList<String> attribut,ArrayList<String> new_attribut) {
+        Boolean ok=false;
+        String requete="UPDATE `hopital`.`service` SET `service`.`";
+            for(int i=0; i<attribut.size();i++){
+                requete=requete+attribut.get(i)+"`='"+new_attribut.get(i)+"'";
+                if(i!=attribut.size()-1){
+                    requete=requete+",`service`.`";
+                }
+            }
+            requete=requete+" WHERE `service`.`code` = '"+code+"';";
+            
+        try {
+            conn.executeUpdate(requete);
+            ok=true;
+        }catch (SQLException ex) {
+            System.out.println("ECHEC REQUETE MODIF: "+requete);
+            Logger.getLogger(Service.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ok;
+    }
+
+    @Override
+    public void setC(Connexion c) {
+        conn=c;
+    }
+
+    @Override
+    public Boolean supprimer() {
+        Boolean ok=false;
+        try {
+            String requete="DELETE FROM `hopital`.`service` WHERE `service`.`code` = '"+code+"';";
+            conn.executeUpdate(requete);
+            ok=true;
+        } catch (SQLException ex) {
+            Logger.getLogger(Chambre.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ok;
+    }
+    
+}
