6925a676b1510434ff152029159ff0793b71b22a
Emmanuelle Thiroloix <emmanuelle.thiroloix@gmail.com>
2018-04-20 11:47:49 +0200

    table

---
+++ Projet_hopital_ING3/src/Modele/BDD/Table.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele.BDD;
+
+import Modele.Connexion;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Emmanuelle THIROLOIX
+ */
+public interface Table {
+    
+    public Boolean supprimer();
+    public void ajouter(String attribut);
+    public boolean modifier(ArrayList<String> attribut,ArrayList<String> new_attribut);
+    public void setC(Connexion c);
+}
